6dbaca69651e2ab8ec4d30b3357984828120b8af
lopeznegrete <lopeznegrete@gmail.com>
2016-04-27 17:44:58 +0200

    Update uart_aci.cpp

--- firmware/uart_aci.cpp
+++ firmware/uart_aci.cpp
@@ -33,9 +33,7 @@ This would mean that the setup cannot be changed once put in.
 However this removes the need to do the setup of the nRF8001 on every reset.
 */
 
-
-#include "nrf8001-ble-uart-spark-io.h"
-
+#include "particle-update.h"
 
 /*
 Externally defined in uart_aci.h
